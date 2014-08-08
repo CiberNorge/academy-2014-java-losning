cbd630e82858e79e8e5591f1d5642132c1da215f
Jorn Anders Svendsen <jorn.anders.svendsen@gmail.com>
2014-08-08 08:49:33 +0200

    JPA fix

--- cibrary/src/main/java/no/ciber/academy/repository/BookDefinitionRepository.java
+++ cibrary/src/main/java/no/ciber/academy/repository/BookDefinitionRepository.java
@@ -1,8 +1,10 @@
 package no.ciber.academy.repository;
 
 import no.ciber.academy.domain.BookDefinition;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-public interface BookDefinitionRepository extends CrudRepository<BookDefinition, Long> {
+@Repository
+public interface BookDefinitionRepository extends JpaRepository<BookDefinition, Long> {
 }
--- cibrary/src/main/java/no/ciber/academy/server/Application.java
+++ cibrary/src/main/java/no/ciber/academy/server/Application.java
@@ -8,14 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Configuration
-@ComponentScan(basePackages = {
-        "no.ciber.academy.domain",
-        "no.ciber.academy.repository",
-        "no.ciber.academy.service",
-        "no.ciber.academy.web.controller"})
 @EnableAutoConfiguration
-@EnableJpaRepositories(basePackages = "no.ciber.academy.repository")
 @Controller
 public class Application {
     public static void main(String[] args) {
