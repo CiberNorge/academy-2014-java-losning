b83f852dcabf676a800caad920dc5088ff024e36
Terje Aasgaard <terje.aasgaard@ciber.com>
2014-08-27 09:51:42 +0200

    mere test

--- src/main/java/no/ciber/academy/Application.java
+++ src/main/java/no/ciber/academy/Application.java
@@ -26,7 +26,7 @@ public class Application {
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 
 
         SimpleDriverDataSource basicDataSource = new SimpleDriverDataSource();
